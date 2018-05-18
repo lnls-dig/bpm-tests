a8b8911de6f0ecdc5a31cc08b2e1f31c08d731bc
Daniel Tavares <daniel.tavares@lnls.br>
2018-05-18 19:09:13 -0300

    logtext.m: add colors to terminal outputs

--- utils/logtext.m
+++ utils/logtext.m
@@ -13,14 +13,22 @@ if nargin == 1
     fid = 1;
     level = 0;
     text = varargin{1};
+    colorized = false;
 elseif nargin == 2
     fid = varargin{1};
     level = 0;
     text = varargin{2};
+    colorized = false;
 elseif nargin == 3
     fid = varargin{1};
     level = varargin{2};
     text = varargin{3};
+    colorized = false;
+elseif nargin == 4
+    fid = varargin{1};
+    level = varargin{2};
+    text = varargin{3};
+    colorized = varargin{4};
 end
 
 level_table = { ...
@@ -35,10 +43,32 @@ leveltxt = level_table{1,1};
 for i=1:length(level_table)
     if (ischar(level) && strcmpi(level, level_table{i,2})) || (isnumeric(level) && level == i-1)
         leveltxt = level_table{i,1};
+        levelnum = i;
         break;
+    else
+        levelnum = -1;
     end
 end
 
 for i=1:length(fid)
-    fprintf(fid(i), '%-5s: [%s] %s\n', leveltxt, datestr(now, 'yy-mm-dd HH:MM:SS'), text);
+    if colorized && fid(i) == 1
+        if levelnum > 2
+            pre = [char(27), '[31m'];
+            post = [char(27), '[0m'];
+        elseif levelnum > 3
+            pre = [char(27), '[33m'];
+            post = [char(27), '[0m'];
+        elseif levelnum > 4
+            pre = [char(27), '[33m'];
+            post = [char(27), '[0m'];
+        else
+            pre = '';
+            post = '';
+        end
+    else
+        pre = '';
+        post = '';
+    end
+
+    fprintf(fid(i), [pre '%-5s: [%s] %s' post '\n'], leveltxt, datestr(now, 'yy-mm-dd HH:MM:SS'), text);
 end

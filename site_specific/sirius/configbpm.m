108cfd7ee969369e9c0444f73712003f801e2376
Daniel Tavares <daniel.tavares@lnls.br>
2018-05-18 19:20:28 -0300

    configbpm.m: add loging and update config files

--- site_specific/sirius/configbpm.m
+++ site_specific/sirius/configbpm.m
@@ -1,59 +1,96 @@
-function configbpm(bpm_config_path)
-%CONFIGBPM   Configure BPMs.
-%   CONFIGBPM(bpm_config_path)
+function configbpm(config_path, crate_number, log_filename)
+%CONFIGBPM   Configure BPMs of a single crate.
+%   CONFIGBPM(config_path, log_filename)
 
 %   Copyright (C) 2017 CNPEM
 %   Licensed under GNU Lesser General Public License v3.0 (LGPL)
 %
 %   Author (Jun-2017): Daniel Tavares (LNLS/DIG) - daniel.tavares@lnls.br
 
-[~, ~, allslotnames] = sirius_bpm_slot_mapping;
-allbpms = allslotnames(4,7:23);
-pbpms = allbpms(1:4);
-rfbpms = allbpms(5:end);
-rfbpms_sr = rfbpms(1:10);
-rfbpms_boo = rfbpms(11:13);
-rfbpms_sp = {};
+if nargin < 3 || isempty(log_filename)
+    fid_logfile = [];
+else
+    fid_logfile = fopen(log_filename, 'a');
+end
 
+fid = [1 fid_logfile];
+
+colorized = true;
+
+filetext = readstrlines(fullfile(config_path, 'bpm', sprintf('names_crate%02d.cfg', crate_number)), '%s %s');
+
+allbpms = filetext{1};
+bpmtypes = filetext{2};
+
+% Filter out inactive BPMs
 config_files_allbpms = { ...
-    'triggers.cfg', ...
+    'monit.cfg', ...
+    %'triggers.cfg', ...
     };
 
-config_files_pbpms = { ...
-    'sirius-frontend-xbpm-debug.cfg', ...
+logtext(fid, 'trace', 'Applying basic configurations and checking active units...');
+allbpms = caputbpmconfig(allbpms, config_files_allbpms, fid, colorized);
+
+% Find BPM types
+pbpms = allbpms(strcmp(bpmtypes, 'pbpm'));
+bpms = allbpms(strcmp(bpmtypes, 'rfbpm-sr') | strcmp(bpmtypes, 'rfbpm-boo') | strcmp(bpmtypes, 'rfbpm-sp'));
+bpms_sr = allbpms(strcmp(bpmtypes, 'rfbpm-sr'));
+bpms_boo = allbpms(strcmp(bpmtypes, 'rfbpm-boo'));
+bpms_sp = allbpms(strcmp(bpmtypes, 'rfbpm-sp'));
+
+config_files_rfbpm = { ...
+    %fullfile(config_path, 'bpm', 'rfbpms', 'backend_reset.cfg')
+    fullfile(config_path, 'bpm', 'rfbpms', 'backend_basic.cfg')
     };
 
-config_files_rfbpms = { ...
-    'backend_reset.cfg', ...
-    'backend_basic.cfg', ...
-    'rffe_basic.cfg', ...
+config_files_rfbpm_rffe = { ...
+    fullfile(config_path, 'bpm', 'rfbpms', 'rffe_basic.cfg')
     };
 
-config_files_rfbpms_sr = { ...
-    'sirius-sr-button_bpm-debug-480mhz.cfg', ...
+config_files_rfbpm_sr = { ...
+    fullfile(config_path, 'bpm', 'rfbpms', 'sr', 'sirius-sr-button_bpm.cfg')
     };
 
-config_files_rfbpms_boo = { ...
-    'sirius-boo-button_bpm-debug-480mhz.cfg', ...
+config_files_rfbpm_boo = { ...
+    fullfile(config_path, 'bpm', 'rfbpms', 'boo', 'sirius-boo-button_bpm.cfg')
     };
 
-config_files_rfbpms_sp = { ...
+config_files_rfbpm_sp = { ...
+    fullfile(config_path, 'bpm', 'rfbpms', 'sp', 'sirius-tl-stripline_bpm.cfg')
     };
 
-caputbpmconfig(allbpms, fullfile(bpm_config_path, 'allbpms'), config_files_allbpms);
-caputbpmconfig(pbpms, fullfile(bpm_config_path, 'pbpms'),   config_files_pbpms);
-caputbpmconfig(rfbpms_sr, fullfile(bpm_config_path, 'rfbpms'),  config_files_rfbpms);
-caputbpmconfig(rfbpms_sr, fullfile(bpm_config_path, 'rfbpms', 'sr'),  config_files_rfbpms_sr);
-caputbpmconfig(rfbpms_boo, fullfile(bpm_config_path, 'rfbpms', 'boo'),  config_files_rfbpms_boo);
-caputbpmconfig(rfbpms_sp, fullfile(bpm_config_path, 'rfbpms', 'sp'),  config_files_rfbpms_sp);
+config_files_pbpm = { ...
+    fullfile(config_path, 'bpm', 'pbpms', 'sirius-frontend-pbpm.cfg')
+    };
 
-function caputbpmconfig(bpmnames, config_path, config_files)
+logtext(fid, 'trace', 'Applying basic BPM configurations and checking active units...');
+caputbpmconfig(bpms, config_files_rfbpm, fid, colorized);
 
-for i=1:length(config_files)
-    aux = readstrlines(fullfile(config_path, config_files{i}), '%s %f');
-    activebpm = caput(buildpvnames(bpmnames, aux{1}(1)), aux{2}(1));
-    activebpm = activebpm == 1; % to overcome MCA limitation (return double instead of logical)
-    bpmnames = bpmnames(activebpm);
-    nbpms = length(bpmnames);
-    caput(buildpvnames(bpmnames, aux{1}(2:end)), repmat(aux{2}(2:end),nbpms,1));
+logtext(fid, 'trace', 'Applying RFFE configurations and checking active units...');
+caputbpmconfig(bpms, config_files_rfbpm_rffe, fid, colorized);
+
+logtext(fid, 'trace', 'Applying Storage Ring BPM configurations and checking active units...');
+caputbpmconfig(bpms_sr, config_files_rfbpm_sr, fid, colorized);
+
+logtext(fid, 'trace', 'Applying Booster BPM configurations and checking active units...');
+caputbpmconfig(bpms_boo, config_files_rfbpm_boo, fid, colorized);
+
+logtext(fid, 'trace', 'Applying Single Pass BPM configurations and checking active units...');
+caputbpmconfig(bpms_sp, config_files_rfbpm_sp, fid, colorized);
+
+logtext(fid, 'trace', 'Applying Photon BPM configurations and checking active units...');
+caputbpmconfig(pbpms, config_files_pbpm, fid, colorized);
+
+if ~isempty(fid_logfile)
+    fclose(fid_logfile);
+end
+
+function [active_units, inactive_units] = caputbpmconfig(bpmnames, config_files, file_id, colorized)
+
+[active_units, inactive_units] = bpm_applyconfig(bpmnames, config_files);
+for i=1:length(bpmnames)
+    logtext(file_id, 'info', sprintf('Active: %s', bpmnames{i}), colorized);
+end
+for i=1:length(inactive_units)
+    logtext(file_id, 'error', sprintf('Inactive: %s', inactive_units{i}), colorized);
 end

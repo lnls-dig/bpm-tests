1ec1751a90997eb4490c5a5d4e9f51af4a44abc4
Daniel Tavares <daniel.tavares@lnls.br>
2018-05-18 19:16:03 -0300

    bpm_readconfig.m: fix unnecessary variable assignment

--- site_specific/sirius/bpm_readconfig.m
+++ site_specific/sirius/bpm_readconfig.m
@@ -1,11 +1,10 @@
 function [param, value, device] = bpm_readconfig(config_files)
 
-config_file = config_files;
 param = {};
 value = [];
 device = {};
-for j=1:length(config_file)
-    aux = readstrlines(config_file{j}, '%s %f %s');
+for j=1:length(config_files)
+    aux = readstrlines(config_files{j}, '%s %f %s');
     param = [param; aux{1}];
     value = [value; aux{2}];
     device = [device; aux{3}];

39be135608a1651b1e2ad641508826d3e451dcb8
Daniel Tavares <daniel.tavares@lnls.br>
2018-05-21 09:31:55 -0300

    configbpm.m: add output argument
    Return BPM names.

--- site_specific/sirius/configbpm.m
+++ site_specific/sirius/configbpm.m
@@ -1,6 +1,6 @@
-function configbpm(config_path, crate_number, log_filename)
+function r = configbpm(config_path, crate_number, log_filename)
 %CONFIGBPM   Configure BPMs of a single crate.
-%   CONFIGBPM(config_path, log_filename)
+%   r = CONFIGBPM(config_path, crate_number, log_filename)
 
 %   Copyright (C) 2017 CNPEM
 %   Licensed under GNU Lesser General Public License v3.0 (LGPL)
@@ -64,22 +64,29 @@ config_files_pbpm = { ...
     };
 
 logtext(fid, 'trace', 'Applying basic BPM configurations and checking active units...');
-caputbpmconfig(bpms, config_files_rfbpm, fid, colorized);
+bpms = caputbpmconfig(bpms, config_files_rfbpm, fid, colorized);
 
 logtext(fid, 'trace', 'Applying RFFE configurations and checking active units...');
-caputbpmconfig(bpms, config_files_rfbpm_rffe, fid, colorized);
+bpms = caputbpmconfig(bpms, config_files_rfbpm_rffe, fid, colorized);
 
 logtext(fid, 'trace', 'Applying Storage Ring BPM configurations and checking active units...');
-caputbpmconfig(bpms_sr, config_files_rfbpm_sr, fid, colorized);
+bpms_sr = caputbpmconfig(bpms_sr, config_files_rfbpm_sr, fid, colorized);
 
 logtext(fid, 'trace', 'Applying Booster BPM configurations and checking active units...');
-caputbpmconfig(bpms_boo, config_files_rfbpm_boo, fid, colorized);
+bpms_boo = caputbpmconfig(bpms_boo, config_files_rfbpm_boo, fid, colorized);
 
 logtext(fid, 'trace', 'Applying Single Pass BPM configurations and checking active units...');
-caputbpmconfig(bpms_sp, config_files_rfbpm_sp, fid, colorized);
+bpms_sp = caputbpmconfig(bpms_sp, config_files_rfbpm_sp, fid, colorized);
 
 logtext(fid, 'trace', 'Applying Photon BPM configurations and checking active units...');
-caputbpmconfig(pbpms, config_files_pbpm, fid, colorized);
+pbpms = caputbpmconfig(pbpms, config_files_pbpm, fid, colorized);
+
+r.allbpms = allbpms;
+r.bpms = bpms;
+r.bpms_sr = bpms_sr;
+r.bpms_boo = bpms_boo;
+r.bpms_sp = bpms_sp;
+r.pbpms = pbpms;
 
 if ~isempty(fid_logfile)
     fclose(fid_logfile);

812ee07e44185ada68c53616a64e64142ae7bc17
Daniel Tavares <daniel.tavares@lnls.br>
2018-05-21 10:56:44 -0300

    exec_test_burnin.m: only plot amplitudes of locked BPMs - improve graph legend

--- site_specific/sirius/tests/test_burnin/exec_test_burnin.m
+++ site_specific/sirius/tests/test_burnin/exec_test_burnin.m
@@ -50,115 +50,146 @@ else
 end
 
 % Check switching
-if ~isempty(bpms)
-    logtext(fid, 'trace', 'Checking if switching works properly...');
-    caput(buildpvnames(bpms, 'SwMode-Sel'), 3);
+if ~isempty(bpms_locked)
+    logtext(fid, 'trace', 'Checking if switching works properly on locked BPMs...');
+    caput(buildpvnames(bpms_locked, 'SwMode-Sel'), 3);
     pause(0.1);
-    [bpms_switching, bpms_notswitching] = bpm_checksw(bpms, swharm_threshold);
+    [bpms_switching, bpms_notswitching] = bpm_checksw(bpms_locked, swharm_threshold);
+    caput(buildpvnames(bpms_locked, 'SwMode-Sel'), 1);
     if isempty(bpms_notswitching)
-        logtext(fid, 'info', sprintf('All active BPMs are switching properly.'));
+        logtext(fid, 'info', sprintf('All active BPMs are switching properly.'),true);
     else
-        logtext(fid, 'error', sprintf('Some BPMs are not switching properly...'));
+        logtext(fid, 'error', sprintf('Some BPMs are not switching properly...'), true);
         for i=1:length(bpms_notswitching)
-            logtext(fid, 'error', sprintf('Not switching: %s', bpms_notswitching{i}));
+            logtext(fid, 'error', sprintf('Not switching: %s', bpms_notswitching{i}),true);
         end
     end
+    
+    fprintf('\n');
+    input('Ready to start amplitude test. Press <ENTER> to continue...');
+    
+    pv_names = buildpvnames(bpms_locked, {'AmplA-Mon', 'AmplC-Mon', 'AmplB-Mon', 'AmplD-Mon'});
+    
+    h = mcaopen(pv_names);
+    nvars = length(h);
+    
+    %clr = lines(nvars);
+    clr = [    0        0        1
+        1        0        0
+        0        1        0
+        0        0   0.1724
+        1   0.1034   0.7241
+        1   0.8276        0
+        0   0.3448        0
+        0.5172   0.5172        1
+        0.6207   0.3103   0.2759
+        0        1   0.7586
+        0   0.5172   0.5862
+        0        0   0.4828
+        0.5862   0.8276   0.3103];
+    
+    mrk = { 'o', '*', '+', 'x'};
+    
+    figure(1);
+    i = 1;
+    for j=1:nvars/4
+        subplot(121);
+        for k=1:4
+            lHandle{i} = line(nan, nan, 'Color', clr(j,:), 'LineWidth', 2, 'Marker', mrk{k});
+            i = i+1;
+        end
+        
+        subplot(122);
+        lHandle{i} = line(nan, nan, 'Color', clr(j,:), 'LineWidth', 2);
+    end
+    xlabel('samples');
+    ylabel('Variation [dB]');
+    
+    subplot(122);
+    legend(bpms_locked);
+    ax = gca;
+    set(ax, 'Visible', 'off');
+    subplot(121);
+    
+    lHandle{nvars+1} = line(nan, nan, 'Color', [0 0 0], 'LineWidth', 5);
+    lHandle{nvars+2} = line(nan, nan, 'Color', [0 0 0], 'LineWidth', 5);
+    
+    yref_inf = 20*log10(1-monit_amp_var_tol);
+    yref_sup = 20*log10(1+monit_amp_var_tol);
+    
+    X = 1:graph_nsamples;
+    Y = nan(graph_nsamples, nvars);
+    
+    ovflw = false;
+    i = 1;
+    while true
+        try
+            newdata = cageth(h);
+            pct = newdata/monit_amp_goal;
+            Y(i,:) = 20*log10(pct);
+            
+            X(i) = i;
+            
+            fprintf('                                  TO/ch1/o      BI/ch2/*      TI/ch3/+      BO/ch4/x  \n', j);
+            fprintf('--------------------------------------------------------------------------------------\n', j);
+            for j=1:nvars/4
+                fprintf('%19s (#%2d)  |', bpms_locked{j}, j);
+                for k=1:4
+                    val = pct(1, (j-1)*4 + k);
+                    if abs(1-val) > monit_amp_var_tol
+                        pattern = ['    ', char(27), '[37;41;1m%9.2f%%', char(27), '[0m'];
+                    else
+                        pattern = '    %9.2f%%';
+                    end
+                    fprintf('%s', sprintf(pattern, val*100));
+                end
+                fprintf('\n');
+            end
+            fprintf('\n');
+            
+            if ovflw
+                for j=1:nvars
+                    set(lHandle{j}, 'XData', X, 'YData', Y([i+1:end 1:i],j));
+                end
+                
+                xs = [0 graph_nsamples+1];
+            else
+                for j=1:nvars
+                    set(lHandle{j}, 'XData', X(1:i), 'YData', Y(1:i,j));
+                end
+                
+                xs = [0 i+1];
+            end
+            
+            set(lHandle{nvars+1}, 'XData', xs, 'YData', [yref_sup yref_sup]);
+            set(lHandle{nvars+2}, 'XData', xs, 'YData', [yref_inf yref_inf]);
+            
+            pause(period_s);
+            
+            i = mod(i, graph_nsamples)+1;
+            
+            if ~ovflw && i == graph_nsamples
+                ovflw = true;
+            end
+        catch
+            break
+        end
+    end
+    
+    mcaclose(h);
 else
+    logtext(fid, 'warning', 'Skipping switching and amplitude tests since there are no locked BPMs...', true);
+    Y = [];
+    X = [];
+    pv_names = [];
     bpms_switching = [];
     bpms_notswitching = [];
 end
 
-pv_names = buildpvnames(bpms_locked, {'AmplA-Mon', 'AmplC-Mon', 'AmplB-Mon', 'AmplD-Mon'});
-
-h = mcaopen(pv_names);
-nvars = length(h);
-
-clr = lines(nvars);
-
-mrk = { 'o', '*', '+', 'x'};
-
-figure(1);
-i = 1;
-for j=1:nvars/4
-    for k=1:4
-        lHandle{i} = line(nan, nan, 'Color', clr(j,:), 'LineWidth', 2, 'Marker', mrk{k});
-        i = i+1;
-    end
-end
-xlabel('samples');
-ylabel('Variation [dB]');
-
-lHandle{nvars+1} = line(nan, nan, 'Color', [0 0 0], 'LineWidth', 5);
-lHandle{nvars+2} = line(nan, nan, 'Color', [0 0 0], 'LineWidth', 5);
-
-yref_inf = 20*log10(1-monit_amp_var_tol);
-yref_sup = 20*log10(1+monit_amp_var_tol);
-
-X = 1:graph_nsamples;
-Y = nan(graph_nsamples, nvars);
-
-ovflw = false;
-i = 1;
-while true
-    try
-        newdata = cageth(h);
-        pct = newdata/monit_amp_goal;
-        Y(i,:) = 20*log10(pct);
-
-        X(i) = i;
-
-        fprintf('                                  TO/ch1/o      BI/ch2/*      TI/ch3/+      BO/ch4/x  \n', j);
-        fprintf('--------------------------------------------------------------------------------------\n', j);
-        for j=1:nvars/4
-            fprintf('%19s (#%2d)  |', bpms_locked{j}, j);
-            for k=1:4
-                val = pct(1, (j-1)*4 + k);
-                if abs(1-val) > monit_amp_var_tol
-                    pattern = ['    ', char(27), '[37;41;1m%9.2f%%', char(27), '[0m'];
-                else
-                    pattern = '    %9.2f%%';
-                end
-                fprintf('%s', sprintf(pattern, val*100));
-            end
-            fprintf('\n');
-        end
-        fprintf('\n');
-
-        if ovflw
-            for j=1:nvars
-                set(lHandle{j}, 'XData', X, 'YData', Y([i+1:end 1:i],j));
-            end
-
-            xs = [0 graph_nsamples+1];
-        else
-            for j=1:nvars
-                set(lHandle{j}, 'XData', X(1:i), 'YData', Y(1:i,j));
-            end
-
-            xs = [0 i+1];
-        end
-
-        set(lHandle{nvars+1}, 'XData', xs, 'YData', [yref_sup yref_sup]);
-        set(lHandle{nvars+2}, 'XData', xs, 'YData', [yref_inf yref_inf]);
-
-        pause(period_s);
-
-        i = mod(i, graph_nsamples)+1;
-
-        if ~ovflw && i == graph_nsamples
-            ovflw = true;
-        end
-    catch
-        break
-    end
-end
-
-mcaclose(h);
-
+raw_results.bpms_locked = bpms_locked;
+raw_results.bpms_notlocked = bpms_notlocked;
+raw_results.bpms_switching = bpms_switching;
+raw_results.bpms_notswitching = bpms_notswitching;
 raw_results.monit_amp = Y;
 raw_results.t = X*period_s;
 raw_results.pv_names = pv_names;
-raw.bpms_locked = bpms_locked;
-raw.bpms_notlocked = bpms_notlocked;
-raw.bpms_switching = bpms_switching;
-raw.bpms_notswitching = bpms_notswitching;

450cbc92cc42e380e0d38e6b912d21b2599b6744
Daniel Tavares <daniel.tavares@lnls.br>
2018-05-21 09:36:34 -0300

    bpm_checksw.m: fix several bugs
    All numerology must be calculated in a vectorized manner since several
    BPMs are evaluated.
    
    The PV names were wrong. Instead of Ampl*-Mon, should be
    GEN_*ArrayData.

--- site_specific/sirius/bpm_checksw.m
+++ site_specific/sirius/bpm_checksw.m
@@ -4,34 +4,27 @@ h = caget(buildpvnames(bpm_names, 'INFOHarmonicNumber-RB'));
 nadc = caget(buildpvnames(bpm_names, 'INFOTBTRate-RB'));
 nsw = 2*caget(buildpvnames(bpm_names, 'SwDivClk-RB'));
 
-sw_adc_factor = round(nsw/nadc);
+sw_adc_factor = round(nsw./nadc);
 
 nyqzone = floor(h./nadc*2);
-if mod(nyqzone,2) == 1
-    nif = (nadc/2 - h + nyqzone.*nadc/2);
-else
-    nif = h - nyqzone.*nadc/2;
+nyqzone_even = mod(nyqzone,2);
+nif = nyqzone_even.*(nadc/2 - h + nyqzone.*nadc/2) + (1-nyqzone_even).*(h - nyqzone.*nadc/2);
+
+nperiods = floor(1e5./nsw);
+
+npts = nperiods.*nsw;
+
+idx_carrier = nif.*sw_adc_factor.*nperiods+1;
+idx_swharm_p1 = (nif.*sw_adc_factor+1).*nperiods+1;
+idx_swharm_m1 = (nif.*sw_adc_factor-1).*nperiods+1;
+    
+wvf_names = {'GEN_AArrayData', 'GEN_CArrayData', 'GEN_BArrayData', 'GEN_DArrayData'};
+
+for i=1:length(bpm_names)
+    r = bpm_acquire(bpm_names(i), wvf_names, 0, npts(i));
+    fft_wvfs = abs(fft(r.wvfs));
+    switching(i) = all((fft_wvfs(idx_swharm_p1(i), :)./fft_wvfs(idx_carrier(i), :) > threshold) & (fft_wvfs(idx_swharm_m1(i), :)./fft_wvfs(idx_carrier(i), :) > threshold));
 end
-
-nperiods = floor(1e5/nsw);
-
-npts = nperiods*nsw;
-
-idx_carrier = nif*sw_adc_factor*nperiods+1;
-idx_swharm_p1 = (nif*sw_adc_factor+1)*nperiods+1;
-idx_swharm_m1 = (nif*sw_adc_factor-1)*nperiods+1;
-
-wvf_names = {'AmplA-Mon', 'AmplC-Mon', 'AmplB-Mon', 'AmplD-Mon'};
-
-r = bpm_acquire(bpm_names, wvf_names, 0, npts);
-
-fft_wvfs = abs(fft(r.wvfs));
-
-switching = (fft_wvfs(:, idx_swharm_p1)./fft_wvfs(:, idx_carrier) > threshold) & (fft_wvfs(:, idx_swharm_m1)./fft_wvfs(:, idx_carrier) > threshold);
-
-nwvfs = size(r.wvfs,2);
-
-switching = all(reshape(switching, 4, nwvfs/4));
 
 bpms_switching = bpm_names(switching);
 bpms_notswitching = bpm_names(~switching);

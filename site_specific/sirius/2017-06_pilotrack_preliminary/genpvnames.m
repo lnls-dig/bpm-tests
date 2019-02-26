86ca62031c088d26f919fe9a773e9a7bd6f32175
Daniel Tavares <daniel.tavares@lnls.br>
2019-02-26 13:01:25 -0300

    remove temporary scripts

--- site_specific/sirius/2017-06_pilotrack_preliminary/genpvnames.m
+++
@@ -1,8 +0,0 @@
-[~,~,all_bpm_names] = sirius_bpm_slot_mapping;
-bpm_names = all_bpm_names(4,11:23);
-
-fid = fopen('pvnames','w+');
-for i=1:length(bpm_names)
-    fprintf(fid, '%s\n', bpm_names{i});
-end
-fclose(fid);

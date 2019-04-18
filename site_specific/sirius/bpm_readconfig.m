c5ffe7b18b344eb771f9dba22aede9dbec44fb29
Daniel Tavares <daniel.tavares@lnls.br>
2019-04-18 15:26:47 -0300

    bpm_readconfig.m: read also waveforms from file

--- site_specific/sirius/bpm_readconfig.m
+++ site_specific/sirius/bpm_readconfig.m
@@ -3,8 +3,15 @@ function [param, value, device] = bpm_readconfig(config_files)
 param = {};
 value = [];
 device = {};
-for j=1:length(config_files)
-    aux = readstrlines(config_files{j}, '%s %f %s');
+for i=1:length(config_files)
+    aux = readstrlines(config_files{i}, '%s %s %s');
+    
+    for j=1:length(aux{2})
+        aux{2}{j} = strrep(aux{2}{j} ,'_',' ');
+        tmpstr = textscan(aux{2}{j}, '%f');
+        aux{2}{j} = tmpstr{1};
+    end    
+    
     param = [param; aux{1}];
     value = [value; aux{2}];
     device = [device; aux{3}];
